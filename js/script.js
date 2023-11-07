ba5a31df67256ebe69f85c537f750f93299887e6
Chikane Mashaba <playtunes100@gmail.com>
2023-11-07 13:30:14 +0200

    Add book constructor

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const myLibrary = [];
 
-function Book() {
+function Book(name, author, pages, read) {
   // the constructor...
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+    this.toggleRead = () => { 
+        this.read ? this.read = false : this.read = true;
+    };
+    
+  
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
